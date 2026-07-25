45e2a31c44b9dfdd8e66a8ceda97be94186006f7
Vadapalli Raviteja <raviteja.vadapalli94@gmail.com>
2026-07-25 12:37:30 +0530

    Fix Compose scope imports

--- android/app/src/main/java/com/movara/app/presentation/MovaraApp.kt
+++ android/app/src/main/java/com/movara/app/presentation/MovaraApp.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.DirectionsCar
 import androidx.compose.material.icons.rounded.GpsFixed
